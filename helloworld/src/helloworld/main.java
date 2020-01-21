78ac2cdafd8675ddbe7f0aa5632c61c111998fc7
ananeamani <ananeamani@192.168.1.5>
2020-01-21 21:27:12 +0100

    comit1

--- helloworld/src/helloworld/main.java
+++ helloworld/src/helloworld/main.java
@@ -4,7 +4,7 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Hello World docker runnig from eclips");
+		System.out.print("Hello World ");
 
 		
 	}
